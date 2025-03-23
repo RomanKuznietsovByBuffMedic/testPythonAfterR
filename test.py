c6024154697f441735f080e8f9b2d1e6e6870119
RomanKuznietsovByBuffMedic <romakuznietsov@ukr.net>
2025-03-23 15:29:36 +0200

    test.py | saving before git add

--- test.py
+++ test.py
@@ -1,49 +1,105 @@
+# Import packeges
+
+# for better print code and result of code
+# https://pypi.org/project/termcolor/
+from termcolor import colored, cprint
+
+
+# define functions
+
+def print_code_and_result(code: str) -> None:
+    '''
+    This function prints the code and the result of the code.
+
+    Args:
+        str (code): The code to execute.
+    Returns:
+        None
+    '''
+    print(colored("Code:", "green"), code)
+    cprint("Result:", "blue")
+    exec(code)
+
+
+def print_chank_of_code(object_title: str, code: str) -> None:
+    '''
+    This function prints the object_title, the code and the result code.
+
+    Args:
+        str (object_title): The object_title of the code.
+        str (code): The code to execute.
+    Returns:
+        None
+    '''
+    print("-----------------------------------------")
+    cprint("TEST " + object_title, "red")
+    print("~~~~~~~~~~~~~~~")
+    print_code_and_result(code)
+    print("~~~~~~~~~~~~~~~\n")
+
+
+# main
+
+print_chank_of_code("`print()`",
+"""
 print("Hello, World!")
-print("ah shit, here we go again")
+print("Ah shit, here we go again")
+""")
 
 
 
+print_chank_of_code("variable",
+"""
 msg = "I'm so tired"
 print(msg)
+""")
 
 
 
+print_chank_of_code("`import`",
+"""
 # https://pypi.org/project/numpy/
 import numpy as np
 
 print(np.random.rand(1, 3))
+""")
 
 
 
+print_chank_of_code("bullean expression",
+"""
 print(1 < 2 and 2)
+""")
 
 
 
+print_chank_of_code("name what start with _",
+"""
 _1 = 1
 print("_1 = ", _1)
+""")
 
 
 
-
-
-
+print_chank_of_code("coment in string",
+"""
 m = "#3" #4
 print(m)
+""")
 
 
 
-
-
-
+print_chank_of_code("string comprassion",
+"""
 print("abc" < "abd")
 print("a" > "A")
 print("z" > "abc")
+""")
 
 
 
-
-
-
+print_chank_of_code("`[]`",
+"""
 word = "hamster"
 
 print("first subsection")
@@ -57,20 +113,20 @@ print(word[1:-1])
 print("third subsection")
 print(word[-3:])
 print(word[:3])
+""")
 
 
 
-
-
-
+print_chank_of_code("type importance",
+"""
 print("1" == 1)
 # print(12 in 123) # TypeError
+""")
 
 
 
-
-
- 
+print_chank_of_code("unit tests by `assert_equal()`",
+""" 
 # https://github.com/python-bakery/bakery-support-library
 from bakery import assert_equal
 
@@ -90,12 +146,12 @@ def calculate_area(length: int, width: int) -> int:
 assert_equal(calculate_area(5, 6), 30)
 assert_equal(calculate_area(0, 6), 0)
 assert_equal(calculate_area(4, 5), 21)
- 
+""") 
 
 
 
-
-
+print_chank_of_code("initialize in `if`",
+"""
 hour = 7
 
 # with `hour <=6` 
@@ -106,23 +162,23 @@ if hour > 6:
     time = "It is dayligtht"
 
 print(time)
+""")
 
 
 
-
-
-
+print_chank_of_code("truthy valuies",
+"""
 # This is False because True is not equal to 5
 print(True == 5)
 
 # This is True because 5 is Truthy
 print(True == bool(5))
+""")
 
 
 
-
-
-
+print_chank_of_code("easy way to create a class by `dataclass`",
+"""
 # Required import
 from dataclasses import dataclass
 
@@ -138,11 +194,13 @@ my_box = Box(5, 10)
 
 # Using the instance
 print(my_box)
+""")
 
 
 
 
-
+print_chank_of_code("test change value of `dataclass`",
+"""
 from dataclasses import dataclass
 
 @dataclass
@@ -156,3 +214,4 @@ print("Ada's name is", ada.name)
 
 ada.name = "adam"
 print("Ada's name is", ada.name)
+""")
